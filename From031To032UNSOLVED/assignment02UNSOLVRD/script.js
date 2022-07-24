a620f4c4f1b9587710c1d390a6a2ad7a1b8742e1
mennaAFathy1014 <menna10awaden@gmail.com>
2022-07-24 21:25:04 +0200

    assignments from 31 to 32

--- From031To032UNSOLVED/assignment02UNSOLVRD/script.js
+++
@@ -1,9 +0,0 @@
-let num1 = 10;
-let num2 = 20;
-
-console.log("Your Code Here"); // true
-console.log("Your Code Here"); // true
-console.log("Your Code Here"); // true
-console.log("Your Code Here"); // true
-console.log("Your Code Here"); // true
-console.log("Your Code Here"); // true
