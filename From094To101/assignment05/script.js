dd4dfc73154222350197f1881b0456885947ef4c
mennaAFathy1014 <menna10awaden@gmail.com>
2022-07-24 09:12:17 +0200

    modified

--- From094To101/assignment05/script.js
+++ From094To101/assignment05/script.js
@@ -1,9 +1,13 @@
 let body = document.querySelector("body");
-let elements = document.body.children;
-console.log(elements);
-
-for(i=0;i<elements.length;i++){
-    elements[i].onclick = function(){
-        console.log(`This Is ${elements[i].nodeName.toLocaleLowerCase()}`);
+let div = document.querySelector("div");
+let span = document.querySelector("span");
+let p = document.querySelector("p");
+let article = document.querySelector("article");
+let section = document.querySelector("section");
+let elemnets = [div,span,p,article,section];
+elemnets.forEach(function(element){
+    element.addEventListener("click",function(){
+        console.log(`this is ${element.tagName.toLocaleLowerCase}`);
     }
-}
+    );
+})
