0e9646f853516bfee43c6b87e120905fd8f2a52c
mennaAFathy1014 <menna10awaden@gmail.com>
2022-07-25 12:24:34 +0200

    modified

--- From040To047/assignment04UNSOLVED/script.js
+++ From040To047/assignment04UNSOLVED/script.js
@@ -1,4 +1,4 @@
 let website = "Go";
 let words = [`${website}ogle`, "Facebook", ["Elzero", "Web", "School"]];
 
-console.log(); // ZERO
+console.log(words.pop().shift().split("").splice(words.indexOf("Facebook")).splice(words.indexOf("Facebook")).join("").toUpperCase()); // ZERO
