64860cbea099dd2aad116e817c9cb0c1d0cb2bf1
mennaAFathy1014 <menna10awaden@gmail.com>
2022-07-25 12:21:28 +0200

    modified

--- From040To047/assignment05/script.js
+++ From040To047/assignment05/script.js
@@ -2,6 +2,12 @@ let needle = "JS";
 let haystack = ["PHP", "JS", "Python"];
 
 // Write 3 Solutions
-console.log(haystack.includes(needle))
-console.log(haystack.indexOf(needle))
-console.log(haystack.lastIndexOf(needle))
+if(haystack.includes(needle))
+{
+    console.log("Found");
+}
+for(let i = 0; i < haystack.length; i++){
+    if(haystack[i] === needle){
+        console.log("Found");
+    }
+}
