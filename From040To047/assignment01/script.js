f61e4ccab179b8c9b2baa42ff087aead24d9ed44
mennaAFathy1014 <menna10awaden@gmail.com>
2022-07-25 12:29:20 +0200

    modified

--- From040To047/assignment01/script.js
+++ From040To047/assignment01/script.js
@@ -2,7 +2,7 @@ let myFriends = ["Ahmed", "Elham", "Osama", "Gamal"];
 let num = 3;
 
 // Method 1
-console.log(myFriends.slice()); // ["Ahmed", "Elham", "Osama"];
+console.log(myFriends.slice(0,num)); // ["Ahmed", "Elham", "Osama"];
 
 // Method 2
-console.log(myFriends); // ["Ahmed", "Elham", "Osama"];
+console.log(myFriends.splice(0,num)); // ["Ahmed", "Elham", "Osama"];
