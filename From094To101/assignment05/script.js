55e07d05ae99a2752776d7d8da8939f087098dc6
mennaAFathy1014 <menna10awaden@gmail.com>
2022-07-25 21:28:18 +0200

    modified

--- From094To101/assignment05/script.js
+++ From094To101/assignment05/script.js
@@ -1,13 +1,4 @@
-let body = document.querySelector("body");
-let div = document.querySelector("div");
-let span = document.querySelector("span");
-let p = document.querySelector("p");
-let article = document.querySelector("article");
-let section = document.querySelector("section");
-let elemnets = [div,span,p,article,section];
-elemnets.forEach(function(element){
-    element.addEventListener("click",function(){
-        console.log(`this is ${element.tagName.toLocaleLowerCase}`);
-    }
-    );
-})
+window.onclick = function(event) {
+    let type = event.target.tagName.toLocaleLowerCase();
+    console.log(`this is ${type}`);
+}
